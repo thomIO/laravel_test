296164a2194e00f64f33f63c4da92a5a0de51730
THOMio <thomearls@gmail.com>
2013-06-09 05:12:25 +0100

    spring clean

--- public/index.php
+++ public/index.php
@@ -68,60 +68,7 @@
 			</li>
 		</ul>
 
-
 	</div> <!-- / #content -->
-
-
-	<script>
-
-	var composerCtrl = function ($scope) {
-
-		$scope.composers = [
-			{f_name: "Russ", l_name: "Boad", location: "Binfield", bio: "A gamer and google fan.", image: "russ.jpg"},
-			{f_name: "Thom", l_name: "Earls", location: "Maidenhead", bio: "A designer and machead.", image: "thom.jpg"},
-			{f_name: "Tarek", l_name: "Karaman", location: "Marlow", bio: "A musician and veggie.", image: "tarek.jpg"},
-			{f_name: "Craig", l_name: "Isaia", location: "Ascot", bio: "Plays the bongos", image: "craig.jpg"},
-			{f_name: "Oli", l_name: "Nako", location: "Addlestone", bio: "Likes rock climbing", image: "oli.jpg"},]
-		$scope.predicate = 'f_name';
-
-		console.log($scope.composers);
-
-
-		$scope.addComposer = function () {
-			$scope.composers.push({
-				f_name: 	$scope.new_f_name,
-				l_name: 	$scope.new_l_name,
-				location: 	$scope.new_location,
-				bio: 		$scope.new_bio,
-				image: 		$scope.new_image
-			});
-			$scope.new_f_name = "";
-			$scope.new_l_name = "";
-			$scope.new_location = "";
-			$scope.new_bio = "";
-			$scope.new_image = "";
-
-			console.log("added a composer");
-		};
-
-		$scope.removeComposer = function (composer) {
-			var id = $scope.composers.indexOf(composer);
-			$scope.composers.splice(id, 1);
-
-			console.log("removed selected composer");
-		};
-
-		$scope.$watch('searchComposer', function (){
-			console.log($scope.searchComposer);
-		});
-
-	
-	};
-
-
-
-	</script>
-
 
 </body>
 
